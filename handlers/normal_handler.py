be313ca2af55d03e4f1b72e941921c7137149119
Singee <imsingee@gmail.com>
2019-12-01 14:43:22 +0800

    isolate upload_to_google_drive

--- handlers/normal_handler.py
+++ handlers/normal_handler.py
@@ -1,10 +1,9 @@
 from telethon import TelegramClient
 from telethon.events.newmessage import NewMessage
-
-from utils.status import set_status, clear_status, check_status
-from utils.authenticate import is_authenticated, get_drive
+from utils.status import check_status
+from utils.authenticate import is_authenticated
 from utils.download import from_telegram as download_from_telegram
-from utils.folders import get_default_folders
+from utils.upload import to_google_drive as upload_to_google_drive
 
 
 def filter(event: NewMessage.Event):
@@ -36,28 +35,7 @@ async def handler(event: NewMessage.Event):
 
         downloaded_media = await download_from_telegram(event.client, event.message, sent_message)
         await sent_message.edit('下载完成，即将上传至 Google Drive')
-        print(downloaded_media)
-        drive = get_drive(from_user_id)
-        file = drive.CreateFile()
-        file['title'] = downloaded_media.split('/')[-1]
-        folders = get_default_folders(from_user_id)
-        if folders:
-            file['parents'] = [{'id': x} for x in folders]
-        file.SetContentFile(downloaded_media)
-        await sent_message.edit('正在上传至 Google Drive。通常情况下 1GB 文件需要 1min 左右，如果在超出预估时间 5 倍后您依然未收到成功或失败的提醒请联系管理员。')
-        try:
-            file.Upload()
-        except Exception as e:
-            # TODO: 允许重试
-            await sent_message.edit('上传失败。\n\n' + str(e))
-            return
-
-        if folders:
-            await sent_message.edit(f"上传完成，文件已经存储至您设置的默认文件夹。\n\n[点此查看此文件]({file['alternateLink']})")
-        else:
-            await sent_message.edit(f"上传完成，文件已经存储至您网盘的根目录。\n\n[点此查看此文件]({file['alternateLink']})")
-        # TODO: 删除上传完成后的文件
-        # TODO: 重命名
+        await upload_to_google_drive(from_user_id, downloaded_media, sent_message)
     else:
         await event.reply(
             '纯文本信息不会被保存至 Google Drive'
---
+++ utils/upload.py
@@ -0,0 +1,31 @@
+from utils.authenticate import get_drive
+from utils.folders import get_default_folders
+
+
+async def to_google_drive(user_id, file_path, sent_message):
+    drive = get_drive(user_id)
+    if not drive:
+        # TODO: 允许重试
+        await sent_message.edit('上传失败。请发送 /authenticate 授权 Google Drive 后重试。')
+        return False
+
+    file = drive.CreateFile()
+    file['title'] = file_path.split('/')[-1]
+    folders = get_default_folders(user_id)
+    if folders:
+        file['parents'] = [{'id': x} for x in folders]
+    file.SetContentFile(file_path)
+    await sent_message.edit('正在上传至 Google Drive。通常情况下 1GB 文件需要 1min 左右，如果在超出预估时间 5 倍后您依然未收到成功或失败的提醒请联系管理员。')
+    try:
+        file.Upload()
+    except Exception as e:
+        # TODO: 允许重试
+        await sent_message.edit('上传失败。\n\n' + str(e))
+        return
+
+    if folders:
+        await sent_message.edit(f"上传完成，文件已经存储至您设置的默认文件夹。\n\n[点此查看此文件]({file['alternateLink']})")
+    else:
+        await sent_message.edit(f"上传完成，文件已经存储至您网盘的根目录。\n\n[点此查看此文件]({file['alternateLink']})")
+    # TODO: 删除上传完成后的文件
+    # TODO: 重命名
