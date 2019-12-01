6a76b4f079b7519d88e9c8bb81cae63c4a70f53d
Singee <imsingee@gmail.com>
2019-12-01 17:06:44 +0800

    support /move

--- handlers/file_action_handler.py
+++ handlers/file_action_handler.py
@@ -3,6 +3,7 @@ import db
 from telethon import TelegramClient
 from telethon.events.newmessage import NewMessage
 from utils.authenticate import get_drive
+from utils.folders import get_folder_id
 
 
 async def rename_handler(event: NewMessage.Event):
@@ -47,5 +48,42 @@ async def rename_handler(event: NewMessage.Event):
     await event.reply(f"重命名成功，新文件名为 [{file['title']}]({file['alternateLink']})")
 
 
+async def move_handler(event: NewMessage.Event):
+    print('move_handler', event)
+    reply_to_msg_id = event.message.reply_to_msg_id
+
+    if not reply_to_msg_id:
+        # TODO - 引导选择文件
+        await event.reply('请【回复】相应消息以移动文件')
+        return
+
+    file_id = db.get('message-info-google-drive-file-id', f'{event.chat.id}.{reply_to_msg_id}')
+    print(f'{event.chat.id}.{reply_to_msg_id}')
+    if not file_id:
+        await event.reply('请回复正确的消息以移动文件')
+        return
+
+    from_user_id = event.message.from_id
+    new_folder_ids = event.message.message.strip().split()[1:]
+    if len(new_folder_ids) == 0:
+        await event.reply('请输入至少一个新文件夹 ID')
+        return
+
+    new_parents = [{'id': get_folder_id(from_user_id, x)} for x in new_folder_ids]
+
+    drive = get_drive(from_user_id)
+    file = drive.CreateFile({'id': file_id})
+    try:
+        file.FetchMetadata(fields='parents')
+        file['parents'] = new_parents
+        file.Upload()
+    except:
+        await event.reply('文件不存在或父文件夹不存在或您无权操作')
+        return
+
+    await event.reply(f"移动成功")
+
+
 def register(client: TelegramClient):
     client.on(NewMessage(pattern='/rename'))(rename_handler)
+    client.on(NewMessage(pattern='/move'))(move_handler)
--- utils/folders.py
+++ utils/folders.py
@@ -4,10 +4,26 @@ import db
 def get_default_folders(user_id):
     default_folders = db.get('default-folders', user_id)
     if default_folders:
-        return default_folders.split()
+        return [get_folder_id(user_id, x) for x in default_folders.split()]
     else:
         return []
 
 
+def clear_default_folders(user_id):
+    db.delete('default-folders', user_id)
+
+
 def set_default_folders(user_id, folders):
     db.set('default-folders', user_id, ' '.join(folders))
+
+
+def get_folder_id(user_id, folder_alias):
+    return db.get('folder-alias', f'{user_id}.{folder_alias}', default=folder_alias)
+
+
+def set_folder_alias(user_id, folder_alias, folder_id):
+    db.set('folder-alias', f'{user_id}.{folder_alias}', folder_id)
+
+
+def delete_folder_alias(user_id, folder_alias):
+    db.delete('folder-alias', f'{user_id}.{folder_alias}')
